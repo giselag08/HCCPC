0bac42f0d05f8ebb274719fbfe6f0e47bf6939bb
Gisela Gonzalez <77498589+giselag08@users.noreply.github.com>
2021-04-09 16:37:10 -0400

    Add files via upload

---
+++ R Functions/LoadPack_and_CSVFiles.R
@@ -0,0 +1,6 @@
+# Load package 
+library(HCCPC)
+
+# Load csv files 
+ILL_2020_stats <- read.csv(file = "~/Desktop/ILL 2020.csv", header = TRUE)
+ILL_2021_stats <- read.csv(file = "~/Desktop/ILL 2021.csv", header = TRUE)
